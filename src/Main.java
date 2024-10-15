e54e73c723a889511042d50a9e0759803ecc622c
Filip Madzia <filipmadzia33@gmail.com>
2024-10-15 18:44:06 +0200

    Test commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        var cat = new Cat("Fika", 9);
+
+        var catAge = cat.getAge();
+
+        System.out.println(catAge);
+    }
+}
