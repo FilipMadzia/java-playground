3714ed0c9c5111548793b735e88d7099e260ed0f
Filip Madzia <filipmadzia33@gmail.com>
2024-10-16 19:06:21 +0200

    Bubble sort

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        var cat = new Cat("Fika", 9);
-
-        var catAge = cat.getAge();
-
-        System.out.println(catAge);
+        System.out.println("Main.java");
     }
 }
