3cf99c80850ab8bda5c8bd623009f6af8ee73893
Filip Madzia <filipmadzia33@gmail.com>
2024-11-10 15:31:34 +0100

    Finished simple calculator

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -6,11 +6,51 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int firstNumber = getNumberSafe("Input first number: ");
-        int secondNumber = getNumberSafe("Input second number: ");
+        int firstNumber = getNumberSafe("Input first number:");
+        int secondNumber = getNumberSafe("Input second number:");
 
-        System.out.println(firstNumber);
-        System.out.println(secondNumber);
+        String action = getActionSafe();
+
+        float result = 0;
+
+        switch(action) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = (float) firstNumber / secondNumber;
+                break;
+            default:
+                System.out.println("Something went wrong");
+        }
+
+        System.out.println(firstNumber + " " + action + " " + secondNumber + " = " + result);
+    }
+
+    static String getActionSafe() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Input action (+, -, *, /):");
+
+        String action = null;
+
+        while(action == null) {
+            action = reader.readLine();
+
+            if(!action.equals("+") && !action.equals("-") && !action.equals("*") && !action.equals("/")) {
+                action = null;
+
+                System.out.println("Not a valid action, try again");
+            }
+        }
+
+        return action;
     }
 
     static int getNumberSafe(String message) throws IOException {
